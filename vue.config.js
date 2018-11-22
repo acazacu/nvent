29b5727ffbb288fc4e5e45196d5779f2e99540a3
acazacu <bed-Gez-SPR-zL6>
2018-11-22 02:23:00 +0100

    Trying to enable devtools on the dev env

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ process.env.VUE_APP_VERSION = require('./package.json').version;
 module.exports = {
     baseUrl: process.env.VUE_CONFIG_BASE_URL,
     productionSourceMap: process.env.VUE_CONFIG_PRODUCTION_SOURCE_MAP === 'true',
+    devtools: true,
     css: {
         extract: process.env.VUE_CONFIG_CSS_EXTRACT === 'true',
         sourceMap: process.env.VUE_CONFIG_CSS_SOURCE_MAP === 'true'
