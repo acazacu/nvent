848ad8b406df69b561d9583b6d5bcea0eee87a10
acazacu <bed-Gez-SPR-zL6>
2018-11-22 03:04:54 +0100

    Removed the test about page

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import About from './views/About.vue';
 
 Vue.use(Router);
 
@@ -14,10 +13,6 @@ export default new Router({
           alias: '/home',
           name: 'home',
           component: Home,
-      },
-      {
-        path: '/about',
-          component: About,
       },
   ],
 });
--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h2>About</h2>
-        <p>This website is built in vuejs</p>
-    </div>
-</template>
